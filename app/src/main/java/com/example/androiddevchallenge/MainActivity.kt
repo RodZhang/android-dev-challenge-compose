ce8f75f796fa04b5e2d2109f45a0d83d28e395b0
Rongdong Zhang <zhangrongdong@bigo.sg>
2021-03-03 12:32:13 +0800

    fix

--- app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
@@ -26,7 +26,7 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.navArgument
 import androidx.navigation.compose.rememberNavController
-import com.example.androiddevchallenge.ui.Home
+import com.example.androiddevchallenge.ui.page.Home
 import com.example.androiddevchallenge.ui.page.PetDetail
 import com.example.androiddevchallenge.ui.theme.MyTheme
 
